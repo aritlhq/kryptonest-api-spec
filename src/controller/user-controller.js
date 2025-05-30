cced5c624621719fe735c83f4db5c6d429cd8fe4
aritlhq <aritlhq@gmail.com>
2025-05-30 23:02:18 +0700

    feat(user): Implement login functionality

--- src/controller/user-controller.js
+++ src/controller/user-controller.js
@@ -12,4 +12,16 @@ const register = async (req, res, next) => {
     }
 }
 
-export default {register};
+const login = async (req, res, next) => {
+    try {
+        const result = await userService.login(req.body);
+
+        res.status(200).json({
+            data: result
+        });
+    } catch (e) {
+        next(e);
+    }
+}
+
+export default {register, login};
