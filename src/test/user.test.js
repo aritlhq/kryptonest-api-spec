54e7f026dfcf957ec1a07c1a6e28c96a90ee0653
aritlhq <aritlhq@gmail.com>
2025-05-30 23:02:12 +0700

    feat(user.test.js): Add login test cases
    Add test cases for login functionality, including success and failure scenarios with invalid credentials. Also, import createTestUser for testing purposes.

--- src/test/user.test.js
+++ src/test/user.test.js
@@ -1,4 +1,4 @@
-import {removeTestUser} from "./test-util.js";
+import {createTestUser, removeTestUser} from "./test-util.js";
 import supertest from "supertest";
 import {web} from "../application/web.js";
 import {logger} from "../application/logging.js";
@@ -6,7 +6,7 @@ import {logger} from "../application/logging.js";
 describe('POST /api/accounts', function () {
     afterEach(async () => {
         await removeTestUser();
-    })
+    });
 
     it('Should be register new user', async () => {
         const result = await supertest(web)
@@ -30,24 +30,72 @@ describe('POST /api/accounts', function () {
     });
 
     /**
+     * Tests Passed:
      * - Empty
-     * - Invalid input length for username
+     * - Invalid input length for field(s)
      * - Invalid format for email
      */
-    it('Should be reject if request is invalid', async() => {
-       const result = await supertest(web)
-           .post('/api/accounts')
-           .send({
-               username: "asdf",
-               first_name: "asdf",
-               last_name: "asdf",
-               email: "asdf@",
-               password: "aszdfasdf",
-           });
+    it('Should be reject if request is invalid', async () => {
+        const result = await supertest(web)
+            .post('/api/accounts')
+            .send({
+                username: "asdf",
+                first_name: "asdf",
+                last_name: "asdf",
+                email: "asdf@",
+                password: "aszdfasdf",
+            });
 
-       logger.info(result.body);
+        logger.info(result.body);
 
-       expect(result.status).toBe(400);
-       expect(result.body.errors).toBeDefined();
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined();
     });
 });
+
+
+describe('POST /api/accounts/login', function () {
+        beforeEach(async () => {
+            await createTestUser();
+        });
+
+        afterEach(async () => {
+            await removeTestUser();
+        });
+
+        it('Should be able to login', async () => {
+            const result = await supertest(web)
+                .post('/api/accounts/login')
+                .send({
+                    email: "testemail@mail.com",
+                    password: "password",
+                });
+
+            logger.info(result.body);
+
+            expect(result.status).toBe(200);
+            expect(result.body.data.token).toBeDefined();
+            expect(result.body.data.token).not.toBe("123456");
+        });
+
+        /**
+         * Tests Passed:
+         * - Empty
+         * - Invalid input length for field(s)
+         * - Invalid email or password
+         */
+        it('Should be reject if request is invalid', async () => {
+            const result = await supertest(web)
+                .post('/api/accounts/login')
+                .send({
+                    email: "testemail@mail.com",
+                    password: "password32",
+                });
+
+            logger.info(result.body);
+
+            expect(result.status).toBe(400);
+            expect(result.body.errors).toBeDefined();
+        });
+    }
+);
