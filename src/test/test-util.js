d1ec5c0cbfd8799dcad6ec27793b65e98499be97
aritlhq <aritlhq@gmail.com>
2025-05-31 14:55:32 +0700

    feat(test-util): Generate JWT token for test user

--- src/test/test-util.js
+++ src/test/test-util.js
@@ -1,5 +1,6 @@
 import {prismaClient} from "../application/database.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 export const removeTestUser = async () => {
     await prismaClient.user.deleteMany({
@@ -13,11 +14,20 @@ export const createTestUser = async () => {
     await prismaClient.user.create({
         data: {
             username: "tester",
-            first_name: "testfirst_name",
-            last_name: "testlast_name",
+            first_name: "test first_name",
+            last_name: "test last_name",
             email: "testemail@mail.com",
             password: await bcrypt.hash("password", 10),
-            token: "123456"
+            // token: token
         }
     });
+
+    const token = jwt.sign({
+        email: "testemail@mail.com",
+        username: "tester",
+    }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "10s"
+    });
+
+    return token;
 }
