8150feaa819f1dc4b4bacf1df5d2134c13e80b5c
aritlhq <aritlhq@gmail.com>
2025-05-31 14:55:47 +0700

    feat(user-validation): Add getUserValidation schema

--- src/validation/user-validation.js
+++ src/validation/user-validation.js
@@ -14,4 +14,10 @@ const loginUserValidation = Joi.object({
     password: Joi.string().max(200).required(),
 });
 
-export {registerUserValidation, loginUserValidation};
+const getUserValidation = Joi.string().max(200).email().required();
+
+export {
+    registerUserValidation,
+    loginUserValidation,
+    getUserValidation
+};
